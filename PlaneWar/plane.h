200a9ef4097599d6a661f2b3b3141f60f7845e75
U202442465 <1727143316@qq.com>
2025-03-30 15:58:51 +0800

    验证了新增类的使用

--- PlaneWar/plane.h
+++ PlaneWar/plane.h
@@ -1,11 +1,22 @@
 #pragma once
 class planeBody {
+private:
+	int x, y;
+	int width, height;
+	int speed;
+	int life;
+	int score;
+	int bulletType;
+	int bulletSpeed;
+	int bulletPower;
+	int bulletNum;
 public:
 	planeBody();
 	~planeBody();
 	void draw();
 	void move(int x, int y);
 	void setPos(int x, int y);
+	void setBulletSpeed(int speed);
 	int getX();
 	int getY();
 };
